d014281c5ad7edf0763e23a500d6fd3fb2e96306
Guosa Edebiri <gedebiri@gmail.com>
2020-04-09 17:12:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,20 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  advice = "It's Dangerous To Go Alone! Take This."
+  puts advice
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
 end
